d4136daabe4fbddcbd973d18e61eedaca98a3699
Lmedeiros-leiman <lmedeiros045@gmail.com>
2023-10-26 13:16:17 -0300

    finalizado codigo permutacoes

--- codewars/SoManyPermutations.java
+++ codewars/SoManyPermutations.java
@@ -1,49 +1,28 @@
 // https://www.codewars.com/kata/5254ca2719453dcc0b00027d/train/csharp
 
-// a ideia é pegar a string original e mostrar todas as permutações possiveis.
-// obrigatório remover as permutações duplicadas
-// ("ab" => ["ab","ba"] )
-//
-//
-//
-//
-
 import java.util.*;
 
 public class SoManyPermutations {
 
-    public static void main(String[] args) {
-        String str = "ABC";
-        List<String> permutations = singlePermutations(str);
-        for (String permutation : permutations) {
-            System.out.println(permutation);
-        }
+    public static void generate(String seq, String out, List<String> list) {
+        if (seq.isEmpty()) {
+            if (!list.contains(out))
+                list.add(out);
+        } else
+            for (int i = 0; i < seq.length(); i++)
+                generate(remove(seq, i), out + seq.charAt(i), list);
+    }
+
+    public static String remove(String str, int idx) {
+        return str.substring(0, idx) + str.substring(idx + 1);
     }
 
     public static List<String> singlePermutations(String s) {
-        List<String> result = new ArrayList<>();
-        permute(s.toCharArray(), 0, s.length() - 1, result);
-        return result;
-    }
-
-    private static void permute(char[] str, int l, int r, List<String> result) {
-        if (l == r) {
-            result.add(new String(str));
-        } else {
-            for (int i = l; i <= r; i++) {
-                str = swap(str, l, i);
-                permute(str, l + 1, r, result);
-                str = swap(str, l, i);
-            }
+        List<String> list = new ArrayList<>();
+        for (int i = 0; i < s.length(); i++) {
+            generate(remove(s, i), "" + s.charAt(i), list);
         }
-    }
-
-    private static char[] swap(char[] a, int i, int j) {
-        char temp = a[i];
-        a[i] = a[j];
-        a[j] = temp;
-        return a;
+        return list;
     }
 
 }
-// This code is contributed by mits
