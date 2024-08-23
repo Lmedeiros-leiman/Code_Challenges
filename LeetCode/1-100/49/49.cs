7d489e9c499b47eb8245fffc843fd23b50ac55ac
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-08-23 18:15:28 -0300

    GroupAnagrams

--- LeetCode/1-100/49/49.cs
+++ LeetCode/1-100/49/49.cs
@@ -1,19 +1,22 @@
+using System.Collections.Generic;
+using System.Linq;
+
 public class Solution {
     public IList<IList<string>> GroupAnagrams(string[] strs) {
-        var groups = new Dictionary<string, IList<string>>();
+        Dictionary<string, IList<string>> anagramGroups = new();
 
         foreach (string s in strs) {
             char[] hash = new char[26];
             foreach (char c in s) {
                 hash[c - 'a']++;
             }
-            
-            string key = new string(hash);
-            if (!groups.ContainsKey(key)) {
-                groups[key] = new List<string>();
+
+            string organizedString = new string(hash);
+            if (!anagramGroups.ContainsKey(organizedString)) {
+                anagramGroups[organizedString] = new List<string>();
             }
-            groups[key].Add(s);
+            anagramGroups[organizedString].Add(s);
         }
-        return groups.Values.ToList();
+        return anagramGroups.Values.ToList();
     }
 }
