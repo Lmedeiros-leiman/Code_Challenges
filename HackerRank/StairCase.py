e938a8aea0a7c7bba7a06620c53db7514dd7ba12
Leonardo <57924586+Lmedeiros-leiman@users.noreply.github.com>
2024-11-04 19:28:25 -0300

    StairCase

---
+++ HackerRank/StairCase.py
@@ -0,0 +1,23 @@
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'staircase' function below.
+#
+# The function accepts INTEGER n as parameter.
+#
+
+def staircase(n):
+    # Write your code here
+    for s in range(1, n+1):
+        print( " "*(n - s) + "#"*s)
+    
+    pass
+
+if __name__ == '__main__':
+   n = int(input().strip())
+   staircase(n)
